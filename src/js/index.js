b9a6cace0bda5c1acecf7eb23b81c4f9833abe8f
Yan <glavnyydirizhor@gmail.com>
2024-09-05 22:33:37 +0300

    feat: about mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 const burger = () => {
+    const main = document.getElementById('main');
     const burger = document.getElementById('burger');
     const menu = document.getElementById('menu');
 
-    burger.addEventListener('click', () => menu.classList.toggle('menu-active'));
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('menu-active');
+        main.classList.toggle('main-menu-active');
+    });
 };
 
 burger();
