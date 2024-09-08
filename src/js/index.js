b10005669b2577a97c5bc00aa2de7671808c7dc7
Yan <glavnyydirizhor@gmail.com>
2024-09-08 14:36:49 +0300

    feat: works desktop

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,35 @@ const burger = () => {
     });
 };
 
+const slider = () => {
+    const sliderInner = document.querySelector('.slider__inner');
+    const countCard = document.querySelectorAll('.card').length;
+
+    const transformValue = 35.11;
+    const transformValueMax = transformValue * (countCard - 3);
+    const ms = 5000;
+
+    let acc = 0;
+    let intervalId;
+
+    const slide = () => {
+        acc += transformValue;
+
+        if (acc > transformValueMax) {
+            acc = 0;
+        }
+
+        sliderInner.style.transform = `translateX(-${acc}%)`;
+    };
+
+    const start = () => (intervalId = setInterval(slide, ms));
+    const pause = () => clearInterval(intervalId);
+
+    sliderInner.addEventListener('mouseover', pause);
+    sliderInner.addEventListener('mouseout', start);
+
+    start();
+};
+
 burger();
+slider();
